0b03d99cdf1a253df3c6b3b2715591915cf211d8
saent-x <torsxperience@outlook.com>
2025-07-03 13:13:39 +0100

    dashboard sidebar ui

---
+++ src/lib/components/avatar-gen.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { createAvatar } from '@dicebear/core';
+  import { botttsNeutral, funEmoji } from '@dicebear/collection';
+  import * as Avatar from '$lib/components/ui/avatar/index.js';
+    import { cn } from '$lib/utils';
+
+  type Props = {
+    class?: string,
+    variant?: string;
+    name: string;
+  };
+
+  let { class: className, variant, name = "" }: Props = $props();
+  let avatar = $state('');
+
+  if (variant === 'bot') {
+    avatar = createAvatar(botttsNeutral, {
+      size: 128
+    }).toDataUri();
+  } else {
+    avatar = createAvatar(funEmoji, {
+      size: 128
+    }).toDataUri();
+  }
+</script>
+
+<Avatar.Root class={cn(className)}>
+  <Avatar.Image src={avatar} alt="avatar" />
+  <Avatar.Fallback class="rounded-lg">
+    {name.charAt(0).toUpperCase()}
+  </Avatar.Fallback>
+</Avatar.Root>
--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-    import "../../app.css";
+  import '../../app.css';
+  import * as Sidebar from '$lib/components/ui/sidebar/index';
+  import AppSidebar from '$lib/components/app-sidebar.svelte';
 
-    const { children } = $props();
+  const { children } = $props();
 </script>
 
-{@render children()}
+<Sidebar.Provider>
+  <AppSidebar />
+  <main class="flex flex-col h-screen w-screen bg-muted p-5">
+    <Sidebar.Trigger />
+    {@render children?.()}
+  </main>
+</Sidebar.Provider>
